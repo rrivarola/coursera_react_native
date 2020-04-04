7792aff2ed331c67410bd5278e4d3d47f8ece028
Raúl Rivarola <raul.rivarola@netviax.com>
2020-04-04 17:13:07 -0300

    Secure Store

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -14,6 +14,7 @@ import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
 import * as Animatable from 'react-native-animatable';
+import Login from './LoginComponent';
 
 const mapStateToProps = state => {
   return {
@@ -65,7 +66,22 @@ YellowBox.ignoreWarnings([
   'Remote debugger is in a background tab which may cause apps to perform slowly',
 ])
 
-
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login }
+}, {
+navigationOptions: ({ navigation }) => ({
+  headerStyle: {
+      backgroundColor: "#512DA8"
+  },
+  headerTitleStyle: {
+      color: "#fff"            
+  },
+  headerTintColor: "#fff",
+  headerLeft: <Icon name="menu" size={24}
+    iconStyle={{ color: 'white' }} 
+    onPress={ () => navigation.toggleDrawer() } />    
+})
+});
 
 const HomeNavigator = createStackNavigator({
   Home: { screen: Home }
@@ -194,6 +210,21 @@ const styles = StyleSheet.create({
 });
 
 const MainNavigator = createDrawerNavigator({
+  Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
   Home: {
       screen: HomeNavigator,
       navigationOptions: {
@@ -289,10 +320,11 @@ const MainNavigator = createDrawerNavigator({
     }
   }
 },
-  {
-    drawerBackgroundColor: '#D1C4E9',
-    contentComponent: CustomDrawerContentComponent
-  });
+{
+  initialRouteName: 'Home',
+  drawerBackgroundColor: '#D1C4E9',
+  contentComponent: CustomDrawerContentComponent
+});
 
 
 
--- ReactNative/confusion/shared/baseUrl.js
+++ ReactNative/confusion/shared/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://192.168.1.42:3000/";
+export const baseUrl = "http://192.168.1.45:3000/";
 
 //json-server --watch db.json --host 192.168.1.42 -d 2000
