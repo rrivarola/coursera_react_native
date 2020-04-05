f498cfe0ab042ddbc586431f8247fe2e6a7198c6
Raúl Rivarola <raul.rivarola@netviax.com>
2020-04-04 22:43:17 -0300

    Image Manipulator

--- ReactNative/confusion/components/LoginComponent.js
+++ ReactNative/confusion/components/LoginComponent.js
@@ -6,6 +6,7 @@ import { baseUrl } from '../shared/baseUrl';
 import * as SecureStore from 'expo-secure-store';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
+import * as   ImageManipulator  from 'expo-image-manipulator';
 
 class LoginTab extends Component {
 
@@ -143,9 +144,23 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({ imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri);
+                //this.setState({ imageUrl: capturedImage.uri });
             }
         }
+
+    }
+
+    processImage = async (imageUri) => {
+        let processedImage = await ImageManipulator.manipulateAsync(
+            imageUri, 
+            [
+                {resize: {width: 400}}
+            ],
+            {format: 'png'}
+        );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri });
 
     }
 
